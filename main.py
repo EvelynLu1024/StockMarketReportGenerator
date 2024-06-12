24b59ff66492a7c5492fd604d76e6341ec618a71
EvelynLu1024 <89069387+EvelynLu1024@users.noreply.github.com>
2024-06-12 17:17:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # 这是一个示例 Python 脚本。
 
-# 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 import os
 from morningtalk_weekly import MorningTalkWeekly
 # 设置工作目录
-os.chdir("F:\\20231027_TaiKang\\20240611_Task28_morningtalk_weekly\\MTWeeklyCode")
+os.chdir("")
 
 # 实例化MorningTalkWeekly类并生成报告
 morningtalk_weekly = MorningTalkWeekly(
